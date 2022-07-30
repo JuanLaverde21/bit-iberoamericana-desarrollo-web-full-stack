da4ccb73b11b181c824c73ce526aa4e2771a6b0b
JuanLaverde21 <jlaverd7@ibero.edu.co>
2022-07-29 20:43:47 -0500

    HTML Class

---
+++ Logica-y-algoritmia/Class 2/3 loops/index.js
@@ -0,0 +1,39 @@
+alert  ("El programa inicio");
+
+/*for ( let x = 0; x <=5; x ++){
+    alert ("la variable vale: " + x);
+}*/
+
+//Calular numeros pares
+/*for ( let x = 0; x <=10; x ++){
+    if( x % 2 === 0){
+        alert ("la variable vale: " + x);   
+    }
+}*/
+
+//Segun un numero ingreseda por el usuario calcular su factorial
+
+/*var numero = parseInt(prompt("Ingrese un numero"));
+var factorial = 1
+
+for(var i = numero; i > 0; i--){
+	factorial = factorial * i;
+}
+
+console.log(factorial);*/
+
+let num = 6;
+/*while (num <= 5){
+    alert ("El numero es: " + num );
+    num = num + 1;//Contador
+}*/
+
+
+do {
+    alert("por aqui paso")
+} while(num <= 5){
+    alert ("El numero es: " + num );
+    num = num + 1
+}
+
+alert ("El programa finalizo")
